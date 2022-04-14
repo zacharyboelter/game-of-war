492e45b2c4d335d0b43cebf73dc7950a16c18822
Zachary Boelter <zboelter@gmail.com>
2022-04-14 15:41:25 -0500

    set innerHTML of cards div to data from API on click of drawCards

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 
 
-newDeckBtn = document.getElementById('new-deck-btn')
-drawBtn = document.getElementById("draw-btn")
+const newDeckBtn = document.getElementById('new-deck-btn')
+const drawBtn = document.getElementById('draw-btn')
+const cards = document.getElementById('cards')
 
 let deckId
-
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
@@ -20,7 +20,11 @@ function drawCards() {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            console.log(data.cards)
+            console.log(data)
+            cards.innerHTML = `
+                <img src="${data.cards[0].image}">
+                <img src="${data.cards[1].image}">
+            `
         })
 }
 

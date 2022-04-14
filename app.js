ec58ca5ca1f0f87ef3828064827beadea76ded0d
Zachary Boelter <zboelter@gmail.com>
2022-04-14 15:21:42 -0500

    drawCards button and function hooked up to API

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 
 
 newDeckBtn = document.getElementById('new-deck-btn')
+drawBtn = document.getElementById("draw-btn")
 
 let deckId
+
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
-        .then(function(data) {
+        .then(data => {
+            console.log(data)
             deckId = data.deck_id
             console.log(deckId)
-            console.log(data)
         })
 }
 
+function drawCards() {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.cards)
+        })
+}
+
+drawBtn.addEventListener('click', drawCards)
 newDeckBtn.addEventListener('click', handleClick)

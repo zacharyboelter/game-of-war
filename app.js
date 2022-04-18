eb4793e26145572c0d64e31ac5568188e4cd5246
Zachary Boelter <zboelter@gmail.com>
2022-04-18 13:00:07 -0500

    determine whether card 1 or card 2 is higher number

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const newDeckBtn = document.getElementById('new-deck-btn')
 const drawBtn = document.getElementById('draw-btn')
 const cards = document.getElementById('cards')
+const oppCard = document.getElementById('opponent-card')
+const playerCard = document.getElementById('opponent-card')
 
 let deckId
 
@@ -20,13 +22,39 @@ function drawCards() {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
-            cards.innerHTML = `
-                <img src="${data.cards[0].image}">
-                <img src="${data.cards[1].image}">
+            console.log(data.cards)
+            document.getElementById("cards").children[0].innerHTML = `
+                <img src=${data.cards[0].image} class="card" />
+            `
+            document.getElementById("cards").children[1].innerHTML = `
+                <img src=${data.cards[1].image} class="card" />
             `
         })
 }
+
+function determineCardWinner(card1, card2) {
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
+    "10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1Value = valueOptions.indexOf(card1.value)
+    const card2Value = valueOptions.indexOf(card2.value)
+
+    if (card1Value > card2Value) {
+        console.log(`Opponent wins this round`)
+    } else if (card1Value < card2Value) {
+        console.log(`You take this round`)
+    } else {
+        console.log(`Tie!`)
+    }
+}
+
+const card1Obj = {
+    value: "7"
+}
+const card2Obj = {
+    value: "7"
+}
+
+determineCardWinner(card1Obj, card2Obj)
 
 drawBtn.addEventListener('click', drawCards)
 newDeckBtn.addEventListener('click', handleClick)

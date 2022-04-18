b62faa5e9a9d5bcdf90d1ecb17e298cd0aaa0fa1
Zachary Boelter <zboelter@gmail.com>
2022-04-18 14:24:22 -0500

    display remaining cards

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const newDeckBtn = document.getElementById('new-deck-btn')
 const drawBtn = document.getElementById('draw-btn')
 const cards = document.getElementById('cards')
 const oppCard = document.getElementById('opponent-card')
-const playerCard = document.getElementById('opponent-card')
+const playerCard = document.getElementById('player-card')
+const whoWonText = document.getElementById('who-won')
+const remainingCards = document.getElementById('num-of-cards-remaining')
 
 let deckId
 
@@ -23,13 +25,18 @@ function drawCards() {
         .then(res => res.json())
         .then(data => {
             console.log(data.cards)
-            document.getElementById("cards").children[0].innerHTML = `
+            cards.children[0].innerHTML = `
                 <img src=${data.cards[0].image} class="card" />
             `
-            document.getElementById("cards").children[1].innerHTML = `
+            cards.children[1].innerHTML = `
                 <img src=${data.cards[1].image} class="card" />
             `
+            const winnerText = determineCardWinner(data.cards[0], data.cards[1])
+            whoWonText.innerHTML = `${winnerText} ${data.cards[0].value} - ${data.cards[1].value}`
+            remainingCards.innerHTML = `Number Of remaining cards: ${data.remaining}`
         })
+
+        
 }
 
 function determineCardWinner(card1, card2) {
@@ -38,23 +45,35 @@ function determineCardWinner(card1, card2) {
     const card1Value = valueOptions.indexOf(card1.value)
     const card2Value = valueOptions.indexOf(card2.value)
 
-    if (card1Value > card2Value) {
-        console.log(`Opponent wins this round`)
-    } else if (card1Value < card2Value) {
-        console.log(`You take this round`)
-    } else {
-        console.log(`Tie!`)
-    }
+    return card1Value > card2Value ? `Opponent wins this round`
+    : card1Value < card2Value ? 'You win this one' 
+    : `War!`
+
+    // if (card1ValueIndex > card2ValueIndex) {
+    //     return whoWonText.innerHTML = `
+    //     <h2>Card 1 wins!</h2>
+    //     `
+    // } else if (card1ValueIndex < card2ValueIndex) {
+    //    return whoWonText.textContent = `
+    //    <h2>Card 2 wins!</h2>
+    //    `
+    // } else {
+    //    return whoWonText.textContent = `
+    //    <h2>War!!!!!</h2>
+    //    `
+    // }
+  
 }
 
 const card1Obj = {
     value: "7"
 }
 const card2Obj = {
-    value: "7"
+    value: "4"
 }
 
-determineCardWinner(card1Obj, card2Obj)
+
 
 drawBtn.addEventListener('click', drawCards)
 newDeckBtn.addEventListener('click', handleClick)
+determineCardWinner(card1Obj, card2Obj)

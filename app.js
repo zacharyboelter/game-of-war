557298547e56ddc574580cd47dc7c6a4bf2311e6
Zachary Boelter <zboelter@gmail.com>
2022-04-18 15:17:46 -0500

    update app

--- app.js
+++ app.js
@@ -1,84 +1,54 @@
-
-
-const newDeckBtn = document.getElementById('new-deck-btn')
-const drawBtn = document.getElementById('draw-btn')
-const cards = document.getElementById('cards')
-const oppCard = document.getElementById('opponent-card')
-const playerCard = document.getElementById('player-card')
-const whoWonText = document.getElementById('who-won')
-const remainingCards = document.getElementById('num-of-cards-remaining')
-
 let deckId
+const cardsContainer = document.getElementById("cards")
+const newDeckBtn = document.getElementById("new-deck")
+const drawCardBtn = document.getElementById("draw-cards")
+const header = document.getElementById("header")
+const remainingText = document.getElementById("remaining")
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
-            console.log(data)
-            remainingCards.innerHTML = `Number Of remaining cards: ${data.remaining}`
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
             deckId = data.deck_id
             console.log(deckId)
         })
 }
 
-function drawCards() {
+newDeckBtn.addEventListener("click", handleClick)
+
+drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            console.log(data.cards)
-            cards.children[0].innerHTML = `
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
+            cardsContainer.children[0].innerHTML = `
                 <img src=${data.cards[0].image} class="card" />
             `
-            cards.children[1].innerHTML = `
+            cardsContainer.children[1].innerHTML = `
                 <img src=${data.cards[1].image} class="card" />
             `
             const winnerText = determineCardWinner(data.cards[0], data.cards[1])
-            whoWonText.innerHTML = `${winnerText} ${data.cards[0].value} - ${data.cards[1].value}`
-            remainingCards.innerHTML = `Number Of remaining cards: ${data.remaining}`
-
-            if (remainingCards === 0) {
-                drawBtn.btn.disabled = true
+            header.textContent = winnerText
+            
+            if (data.remaining === 0) {
+                drawCardBtn.disabled = true
             }
         })
+})
 
-        
-}
 
 function determineCardWinner(card1, card2) {
     const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
     "10", "JACK", "QUEEN", "KING", "ACE"]
-    const card1Value = valueOptions.indexOf(card1.value)
-    const card2Value = valueOptions.indexOf(card2.value)
-
-    return card1Value > card2Value ? `Opponent wins this round`
-    : card1Value < card2Value ? 'You win this one' 
-    : `War!`
-
-    // if (card1ValueIndex > card2ValueIndex) {
-    //     return whoWonText.innerHTML = `
-    //     <h2>Card 1 wins!</h2>
-    //     `
-    // } else if (card1ValueIndex < card2ValueIndex) {
-    //    return whoWonText.textContent = `
-    //    <h2>Card 2 wins!</h2>
-    //    `
-    // } else {
-    //    return whoWonText.textContent = `
-    //    <h2>War!!!!!</h2>
-    //    `
-    // }
-  
+    const card1ValueIndex = valueOptions.indexOf(card1.value)
+    const card2ValueIndex = valueOptions.indexOf(card2.value)
+    
+    if (card1ValueIndex > card2ValueIndex) {
+        return "Card 1 wins!"
+    } else if (card1ValueIndex < card2ValueIndex) {
+        return "Card 2 wins!"
+    } else {
+        return "War!"
+    }
 }
-
-const card1Obj = {
-    value: "7"
-}
-const card2Obj = {
-    value: "4"
-}
-
-
-
-drawBtn.addEventListener('click', drawCards)
-newDeckBtn.addEventListener('click', handleClick)
-determineCardWinner(card1Obj, card2Obj)
